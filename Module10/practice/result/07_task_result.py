f30955916f83ee4855c6a6d22be0fdcdc4e3a5f3
r41n <89096255+r41n@users.noreply.github.com>
2021-08-28 02:02:43 +0300

    Update 07_task_result.py

--- Module10/practice/result/07_task_result.py
+++ Module10/practice/result/07_task_result.py
@@ -1,1 +1,9 @@
 # Напишите программу генерирующую и записывающую в файле произвольное 20000-значное целое число.
+
+import random
+
+num = random.randint(20000, 29999)
+numbers = str(num)
+
+with open("random_int.txt", "w") as f:
+    f.writelines(numbers)
