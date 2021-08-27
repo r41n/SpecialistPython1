7d915c3d0685c055a45468fdb954ab52ccca2525
r41n <89096255+r41n@users.noreply.github.com>
2021-08-28 01:25:12 +0300

    Update 09_task_result.py

--- Module10/practice/result/09_task_result.py
+++ Module10/practice/result/09_task_result.py
@@ -1,3 +1,15 @@
 # Напишите функцию принимающую на вход целое числом n. И возвращающую список из n элементов.
 # заполненный случайными целыми числами в диапазоне от a до b.
 # Примечание: для генерации случайного числа используйте import random
+
+import random
+
+
+def gen_random_list(size, min_value=-20, max_value=20):
+    list_el = []
+    for _ in range(size):
+        list_el.append(random.randint(min_value, max_value))
+    return list_el
+
+
+print(gen_random_list(25))
